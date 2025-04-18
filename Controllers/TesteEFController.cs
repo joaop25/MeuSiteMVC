16bd5e0fc8ca3cc27d9d4b1c6c14c3aa04672843
João Pedro Fernandes <joao.fernandes@groupsoftware.com.br>
2025-04-18 07:47:29 -0300

    Testes EF

---
+++ Controllers/TesteEFController.cs
@@ -0,0 +1,41 @@
+﻿using MeuSiteMVC.Data;
+using MeuSiteMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuSiteMVC.Controllers
+{
+    public class TesteEFController : Controller
+    {
+        public AppDbContext Db { get; set; }
+        public TesteEFController(AppDbContext db)
+        {
+            Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Joao",
+                Email = "joao@gmail.com",
+                DataNascimento = new DateTime(2000, 03, 25),
+                Avaliacao = 5,
+                Ativo = true
+            };
+
+            //Db.Add(aluno);
+            //Db.SaveChanges();
+
+            var result = Db.Alunos.Where(p => p.Nome.Contains("Jo")).FirstOrDefault();
+            //result.Email = "joaop@gmail.com";
+
+            //Db.Alunos.Update(result);
+            //Db.SaveChanges();
+
+            Db.Alunos.Remove(result);
+            Db.SaveChanges();
+
+            return Content("Teste");
+        }
+    }
+}
---
+++ Migrations/20250418102821_01MeuSiteMVC.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeuSiteMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class _01MeuSiteMVC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
