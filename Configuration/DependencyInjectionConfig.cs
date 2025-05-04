af5a8ca043315c2b581efad6ab92e66e40cb9316
Joao Pedro Fernandes <82421334+joaop25@users.noreply.github.com>
2025-05-04 11:32:15 -0300

    Alterações (#10)
    Co-authored-by: João Pedro Fernandes <joao.fernandes@groupsoftware.com.br>

---
+++ Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,23 @@
+﻿using MeuSiteMVC.Services;
+
+namespace MeuSiteMVC.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static WebApplicationBuilder AddDependencyInjectionConfiguration(this WebApplicationBuilder builder)
+        {
+
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            builder.Services.AddTransient<IOperacaoTransiente, Operacao>();
+            builder.Services.AddScoped<IOperacaoScoped, Operacao>();
+            builder.Services.AddSingleton<IOperacaoSingleton, Operacao>();
+            builder.Services.AddSingleton<IOperacaoSingletonInstance>(new Operacao(Guid.Empty));
+
+
+            builder.Services.AddTransient<OperacaoService>();
+
+            return builder;
+        }
+    }
+}
---
+++ Configuration/MvcConfig.cs
@@ -0,0 +1,86 @@
+﻿using MeuSiteMVC.Data;
+using MeuSiteMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuSiteMVC.Configuration
+{
+    public static class MvcConfig
+    {
+        public static WebApplicationBuilder AddMvcConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Configuration
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            builder.Services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
+
+            builder.Services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
+
+            builder.Services.AddDbContext<AppDbContext>(o =>
+                o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(60);
+                options.ExcludedHosts.Add("example.com");
+                options.ExcludedHosts.Add("www.example.com");
+            });
+
+            return builder;
+        }
+
+        public static WebApplication UseMvcConfiguration(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapAreaControllerRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+            app.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Gestao}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapRazorPages();
+
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                var singService = services.GetRequiredService<IOperacaoSingleton>();
+
+            }
+
+            return app;
+        }
+    }
+}
