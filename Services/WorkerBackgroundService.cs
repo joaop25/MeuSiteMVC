172d5104e4984d5ac8a09a763cc1118ffcc36569
Joao Pedro Fernandes <82421334+joaop25@users.noreply.github.com>
2025-05-15 20:04:03 -0300

    Feature/123 (#14)
    * Alterações
    
    * .
    
    * Adicionando Logs
    
    * Alterando index alunos
    
    * Commit
    
    ---------
    
    Co-authored-by: João Pedro Fernandes <joao.fernandes@groupsoftware.com.br>

---
+++ Services/WorkerBackgroundService.cs
@@ -0,0 +1,14 @@
+﻿namespace MeuSiteMVC.Services
+{
+    public class WorkerBackgroundService : BackgroundService
+    {
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+        }
+    }
+}
