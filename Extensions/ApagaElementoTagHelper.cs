47109be6f94d33b603a2c9caefe28d627de4686c
Joao Pedro Fernandes <82421334+joaop25@users.noreply.github.com>
2025-07-25 20:18:15 -0300

    Tarefa 31 alterando descricao (#32)
    * Mudando descrição exception
    
    * Mudando mensagem propriedade aluno
    
    ---------
    
    Co-authored-by: Joao <joao.fernades@groupsoftware.com.br>

--- Extensions/ApagaElementoTagHelper.cs
+++ Extensions/ApagaElementoTagHelper.cs
@@ -26,7 +26,7 @@ namespace MeuSiteMVC.Extensions
             if (output == null)
                 throw new ArgumentNullException(nameof(output));
             if(string.IsNullOrEmpty(IdentityClaimName) || string.IsNullOrEmpty(IdentityClaimValue))
-                throw new ArgumentException("Teste2.");
+                throw new ArgumentException("Teste3.");
 
             var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
 
